8e8376ef566535c5ee1a12f1ce78671388e101fa
Jeff Cadieux <jeffcadieux@hotmail.com>
2020-11-06 11:00:08 +0900

    Lesson395 Refactor routes using chaining

--- app.js
+++ app.js
@@ -24,31 +24,30 @@ const Article = mongoose.model('Article', articleSchema)
 const port = 3000
 app.listen(port, () => console.log(`Server listening on port ${port}.`))
 
-app.get('/articles', async (req, res) => {
-    try {
-        const foundArticles = await Article.find()
-        res.send(foundArticles)
-    } catch (err) {
-        res.send(err)
-    }
-})
-
-app.post('/articles', async (req, res) => {
-    const { title, content } = req.body
-    const newArticle = new Article({ title, content })
-    try {
-        await newArticle.save()
-        res.send('Article received and added to the database!')
-    } catch (err) {
-        res.send(err)
-    }
-})
-
-app.delete('/articles', async (req, res) => {
-    try {
-        await Article.deleteMany()
-        res.send('All articles deleted!')
-    } catch (err) {
-        res.send(err)
-    }
-})
+app.route('/articles')
+    .get(async (req, res) => {
+        try {
+            const foundArticles = await Article.find()
+            res.send(foundArticles)
+        } catch (err) {
+            res.send(err)
+        }
+    })
+    .post(async (req, res) => {
+        const { title, content } = req.body
+        const newArticle = new Article({ title, content })
+        try {
+            await newArticle.save()
+            res.send('Article received and added to the database!')
+        } catch (err) {
+            res.send(err)
+        }
+    })
+    .delete(async (req, res) => {
+        try {
+            await Article.deleteMany()
+            res.send('All articles deleted!')
+        } catch (err) {
+            res.send(err)
+        }
+    })
